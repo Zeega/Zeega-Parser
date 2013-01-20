e23299d9b9df02ebb1e1d4e2760a2403bb79dedf
jbergen <jbergen@gmail.com>
2013-01-20 17:26:43 -0500

    moved data parsers into the parser repo

--- parser.js
+++ parser.js
@@ -3,13 +3,34 @@ define([
     "zeega",
     "lodash",
     // modules
-    "zeega_parser/modules/project"
+    "zeega_parser/modules/project",
+    "zeega_parser/data-parsers/_all"
 ],
 
-function( Zeega, _, Project ) {
+function( Zeega, _, Project, DataParser ) {
 
-    return function( data, options ) {
-        return new Project( data, options );
+    return function( response, options ) {
+        var parsed;
+console.log(options)
+        // determine which parser to use
+        _.each( DataParser, function( p ) {
+            if ( p.validate( response ) ) {
+                if ( options.debugEvents ) {
+                    console.log( "parsed using: " + p.name );
+                }
+                // parse the response
+                options.parser = p.name;
+                parsed = p.parse( response, options );
+                return false;
+            }
+        }.bind( this ));
+
+        if ( parsed !== undefined ) {
+            return new Project( parsed, options );
+        } else {
+            throw new Error("Valid parser not found");
+        }
+        
     };
 
 });
