04271e477a4701e1c730d2551f9256dc887d84a9
jbergen <jbergen@gmail.com>
2013-01-21 01:29:07 -0500

    rm faulty logic

--- parser.js
+++ parser.js
@@ -22,22 +22,13 @@ function( Zeega, _, ProjectModel, DataParser ) {
                 }
                 options.parser = p.name;
 
-                // is the data already a zeega project model? if so, then just pass it through
-                if ( p.name == "zeega-project-model" ) {
-                    return false;
-                } else {
-                    // parse the data
-                    parsed = p.parse( data, options );
-                }
+                // parse the data
+                parsed = p.parse( data, options );
                 return false;
             }
         }, this );
 
-        // pass the data model through.
-        // TODO: what to do about adding or modifying options?
-        if ( options.parser = "zeega-project-model" ) {
-            return data;
-        } else if ( parsed !== undefined ) {
+        if ( parsed !== undefined ) {
             return new ProjectModel( parsed, options );
         } else {
             throw new Error("Valid parser not found");
