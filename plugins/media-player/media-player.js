fe00f25d8f013723b07a874b2738dfb2705199ff
Joseph Bergen <jbergen@gmail.com>
2013-01-23 18:21:08 -0500

    updated layers and media player

--- plugins/media-player/media-player.js
+++ plugins/media-player/media-player.js
@@ -592,7 +592,7 @@ function(Zeega) {
         },
         updateElapsed: function() {
             var elapsed = this.popcorn.currentTime();
-            console.log('update', elapsed);
+
             this.$(".media-time-elapsed").html( convertTime( elapsed ) );
             this.$(".media-scrubber").slider("value", elapsed);
         },
