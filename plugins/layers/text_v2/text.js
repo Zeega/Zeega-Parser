05091fe13ea57ac58a23114c443fb277bcbdf8bb
jbergen <jbergen@gmail.com>
2013-05-01 11:21:53 -0700

    updated text for mobile pos

--- plugins/layers/text_v2/text.js
+++ plugins/layers/text_v2/text.js
@@ -99,11 +99,11 @@ function( app, _Layer, Visual, TextModal ) {
 
         init: function() {
             console.log("text", app.attributes)
-            if ( app.attributes.mobile ) {
-                this.visualProperties = [
-                    "opacity"
-                ]
-            }
+            // if ( app.attributes.mobile ) {
+            //     this.visualProperties = [
+            //         "opacity"
+            //     ]
+            // }
         },
 
         visualProperties: [
@@ -121,39 +121,46 @@ function( app, _Layer, Visual, TextModal ) {
         saveContent: null,
 
         applyStyles: function() {
-            // if ( app.attributes.mobile ) {
-            //     this.$el.css({
-            //         width: (window.innerWidth - 60 ) + "px",
-            //         left: 0,
-            //         right: 0,
-            //         margin: "auto"
-            //     });
-            // } else {
+            if ( app.attributes.mobile ) {
+                this.$el.css({
+                    width: (window.innerWidth - 60 ) + "px",
+                    left: 0,
+                    right: 0,
+                    margin: "auto"
+                });
+            } else {
                 this.$el.css({
                     left: this.getAttr("left") + "%",
                     width: this.getAttr("width") + "%"
                 });
-            // }
+            }
         },
 
-        // moveOnStage: function() {
-        //     if ( app.attributes.mobile ) {
-        //         this.$el.css({
-        //             width: (window.innerWidth - 60 ) + "px",
-        //             top: "calc(50% - " + this.$el.height() / 2 + "px )",
-        //             left: 0,
-        //             right: 0,
-        //             margin: "auto"
-        //         });
-        //     } else {
-        //         console.log("APPLY WRONG")
-        //         this.$el.css({
-        //             top: this.getAttr("top") + "%",
-        //             left: this.getAttr("left") + "%"
-        //         });
-        //     }
+        moveOnStage: function() {
+            if ( app.attributes.mobile ) {
+                this.$el.css({
+                    width: (window.innerWidth - 60 ) + "px",
+                    top: "calc(50% - " + this.$el.height() / 2 + "px )",
+                    left: 0,
+                    right: 0,
+                    margin: "auto",
+                    color: this.model.get("attr").color,
+                    fontWeight: this.model.getAttr("bold") ? "bold" : "normal",
+                    fontStyle: this.model.getAttr("italic") ? "italic" : "normal",
+                    fontFamily: this.model.getAttr("fontFamily"),
+                    fontSize: this.model.getAttr("fontSize") + "%",
+                    textAlign: this.model.getAttr("textAlign"),
+                    lineHeight: this.model.getAttr("lineHeight") + "em"
+                });
+            } else {
+                console.log("APPLY WRONG")
+                this.$el.css({
+                    top: this.getAttr("top") + "%",
+                    left: this.getAttr("left") + "%"
+                });
+            }
 
-        // },
+        },
 
         updateStyle: function() {
             this.$(".visual-target").text( this.model.getAttr("content") );
