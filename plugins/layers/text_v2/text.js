dff54d47afb26340454d6bb4b1cd2c60306bba99
James Burns <jameburn@gmail.com>
2013-05-27 19:32:40 -0700

    check for undefined to_frame attribute

--- plugins/layers/text_v2/text.js
+++ plugins/layers/text_v2/text.js
@@ -209,7 +209,8 @@ function( app, _Layer, Visual, TextModal ) {
                 });
             }
 
-            if ( !_.isNull( this.getAttr("to_frame") ) ) {
+            if ( !_.isNull( this.getAttr("to_frame")) && !_.isUndefined ( this.getAttr("to_frame") ) ) {
+
                 this.$el.addClass("linked-layer link-reveal");
                 setTimeout(function() {
                     this.$el.removeClass("link-reveal");
