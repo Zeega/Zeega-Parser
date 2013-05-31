8ded19cd0a2564f0461be201ead08ccd4ef78e3f
James Burns <jameburn@gmail.com>
2013-05-31 16:10:38 -0700

    update for new project response format

--- data-parsers/zeega-project.js
+++ data-parsers/zeega-project.js
@@ -10,7 +10,7 @@ function() {
     Parser[ type ] = { name: type };
 
     Parser[ type ].validate = function( response ) {
-        if ( response.sequences && response.frames && response.layers ) {
+        if ( response.project.sequences && response.project.frames && response.project.layers ) {
             return true;
         }
         return false;
@@ -27,39 +27,40 @@ function() {
 
     // no op. projects are already formatted
     Parser[type].parse = function( response, opts ) {
+        removeDupeSoundtrack( response.project );
+        return response.project;
 
-        removeDupeSoundtrack( response );
 
-        if ( opts.endPage ) {
-            var endId, lastPageId, lastPage, endPage, endLayers;
+        // if ( opts.endPage ) {
+        //     var endId, lastPageId, lastPage, endPage, endLayers;
 
-            endId = -1;
-            lastPageId = response.sequences[0].frames[ response.sequences[0].frames.length - 1 ];
-            lastPage = _.find( response.frames, function( frame ) {
-                return frame.id == lastPageId;
-            });
-            endPage = _.extend({}, lastPage );
+        //     endId = -1;
+        //     lastPageId = response.sequences[0].frames[ response.sequences[0].frames.length - 1 ];
+        //     lastPage = _.find( response.frames, function( frame ) {
+        //         return frame.id == lastPageId;
+        //     });
+        //     endPage = _.extend({}, lastPage );
 
-            // only allow images, color layers
-            endLayers = _.filter(response.layers, function( layer ) {
-                return _.include(["Image", "Rectangle"], layer.type ) && _.include( endPage.layers, layer.id );
-            });
+        //     // only allow images, color layers
+        //     endLayers = _.filter(response.layers, function( layer ) {
+        //         return _.include(["Image", "Rectangle"], layer.type ) && _.include( endPage.layers, layer.id );
+        //     });
 
-            endPage.layers = _.pluck( endLayers, "id");
-            endPage.layers.push( endId );
+        //     endPage.layers = _.pluck( endLayers, "id");
+        //     endPage.layers.push( endId );
 
-            // add layer to layer array
-            response.layers.push({
-                id: endId,
-                type: "EndPageLayer"
-            });
+        //     // add layer to layer array
+        //     response.layers.push({
+        //         id: endId,
+        //         type: "EndPageLayer"
+        //     });
             
-            endPage.id = endId;
-            response.frames.push( endPage );
-            response.sequences[0].frames.push( endId );
-        }
+        //     endPage.id = endId;
+        //     response.frames.push( endPage );
+        //     response.sequences[0].frames.push( endId );
+        // }
 
-        return response;
+        // return response;
     };
 
     return Parser;
