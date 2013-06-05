ccb577a778743a3348b882dca99726e819477f24
James Burns <jameburn@gmail.com>
2013-06-04 22:56:39 -0700

    fix parser

--- data-parsers/zeega-project.js
+++ data-parsers/zeega-project.js
@@ -7,16 +7,20 @@ function() {
     Parser[ type ] = { name: type };
 
     Parser[ type ].validate = function( response ) {
+        var project = response.project;
 
-        if ( response.sequences && response.frames && response.layers ) {
+        if ( project.sequences && project.frames && project.layers ) {
             return true;
         }
         return false;
     };
 
-    // no op. projects are already formatted
+
     Parser[type].parse = function( response, opts ) {
-        return response;
+        
+        return response.project;
+
+
     };
 
     return Parser;
